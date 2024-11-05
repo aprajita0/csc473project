8593730617b3cc72df228cbc2747942fff3cfd3b
zehdi02 <de.melchizedek.castro@gmail.com>
2024-11-05 13:50:58 -0500

    styled Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,20 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='nav'>
-        <Link to='/'>Home</Link>
-        <Link to='/trading-hub'>Trade Hub</Link>
-        <Link to='/catalog'>Catalog</Link>
-        {/* <Link to='/search'>Search</Link> */}
-        <Link to='/messages'>Messages</Link>
-        <Link to='/profile'>Profile</Link>
+    <div className='nav flex justify-between items-center px-16 py-8 '>
+        <div className='text-lg text-slate-600 font-black'>MyIdolList</div>
+        <div className='text-sm text-slate-500 font-medium flex space-x-8'>
+          <Link to='/'>Home</Link>
+          <div className='text-slate-400'>|</div>
+          <Link to='/trading-hub'>Trade Hub</Link>
+          <div className='text-slate-400'>|</div>
+          <Link to='/catalog'>Catalog</Link>
+          <div className='text-slate-400'>|</div>
+          {/* <Link to='/search'>Search</Link> */}
+          <Link to='/messages'>Messages</Link>
+          <div className='text-slate-400'>|</div>
+          <Link to='/profile'>Profile</Link>
+        </div>
     </div>
   )
 }
