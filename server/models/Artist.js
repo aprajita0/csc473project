b7ce58cc3218dc303b7b9289cf18dc92bb110b1d
Aprajita Srivastava <98621004+aprajita0@users.noreply.github.com>
2024-11-05 19:34:52 -0500

    Update Artist.js
    Updated Artist model

--- server/models/Artist.js
+++ server/models/Artist.js
@@ -1,15 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const ArtistSchema = new mongoose.Schema({
-    username: { type: String, required: true, maxlength: 25 },
-    password: { type: String, required: true},
-    email: { type: String, required: true},
-    profile_pic: { type: String}, 
-    full_name: { type: String, maxlength: 50 },
-    bio: { type: String, maxlength: 300 },
-    address_line1: { type: String, maxlength: 100 },
-    address_line2: { type: String, maxlength: 100 }
+const ArtistSchema = new mongoose.Schema({ 
+    artist_name: { type: String, maxlength: 70 }
   });
   
   module.exports = mongoose.model('Artist', ArtistSchemaSchema);
