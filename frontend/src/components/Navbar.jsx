43cbbbdbc974238441ab948828d1b77d2368cdfc
zehdi02 <de.melchizedek.castro@gmail.com>
2024-11-06 00:18:20 -0500

    updated Navbay styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,27 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className="Navbar bg-white  flex justify-between items-center px-16 py-8 fixed top-0 left-0 right-0 z-50 shadow-md">
-        <div className='text-lg text-slate-600 font-black'>MyIdolList</div>
-        <div className='text-sm text-slate-500 font-medium flex space-x-8'>
-          <Link to='/'>Home</Link>
-          <div className='text-slate-400'>|</div>
-          <Link to='/trading-hub'>Trade Hub</Link>
-          <div className='text-slate-400'>|</div>
-          <Link to='/catalog'>Catalog</Link>
-          <div className='text-slate-400'>|</div>
-          {/* <Link to='/search'>Search</Link> */}
-          <Link to='/messages'>Messages</Link>
-          <div className='text-slate-400'>|</div>
-          <Link to='/profile'>Profile</Link>
-        </div>
+      <div className='text-2xl text-slate-600 font-black hover:scale-110'>
+        <Link to='/'>MyIdolList</Link>
+      </div>
+
+      <div className='text-sm text-slate-600 font-medium flex space-x-4'>
+        <Link className='hover:underline' to='/'>Home</Link>
+        <div className='text-slate-400'>|</div>
+
+        <Link className='hover:underline' to='/trading-hub'>Trade Hub</Link>
+        <div className='text-slate-400'>|</div>
+
+        <Link className='hover:underline' to='/catalog'>Catalog</Link>
+        <div className='text-slate-400'>|</div>
+
+        {/* <Link to='/search'>Search</Link> */}
+
+        <Link className='hover:underline' to='/messages'>Messages</Link>
+        <div className='text-slate-400'>|</div>
+
+        <Link className='hover:underline' to='/profile'>Profile</Link>
+      </div>
     </div>
   )
 }
