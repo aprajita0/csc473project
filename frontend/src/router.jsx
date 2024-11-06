e46c95f52d651a08adbe9fb517ced751ffcbc4ca
Jezlea Ortega <124209539+XJezleaX@users.noreply.github.com>
2024-11-06 17:35:47 -0500

    Update router.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,6 +4,8 @@ import Catalog from './routes/Catalog.jsx'
 import TradingHub from './routes/TradingHub.jsx'
 import Messages from './routes/Messages.jsx'
 import UserProfile from './routes/UserProfile.jsx'
+import Login from './routes/Login.jsx'
+import Register from './routes/Register.jsx'
 import UserTradePost from "./routes/UserTradePost.jsx"
 
 export const router = createBrowserRouter([
@@ -13,6 +15,8 @@ export const router = createBrowserRouter([
     { path:'/trading-hub', element:<TradingHub /> },
     { path:'/messages', element:<Messages /> },
     { path:'/profile', element:<UserProfile /> },
+    { path:'/Login', element:<Login/> },
+    { path:'/Register', element:<Register/> },
 
     { path:'/tradepost', element:<UserTradePost /> },
 
