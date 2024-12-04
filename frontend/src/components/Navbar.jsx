610ae2c0e2a1ab80a1cc55fbf4b684feb6fdbcc2
Jezlea Ortega <124209539+XJezleaX@users.noreply.github.com>
2024-12-03 19:24:21 -0500

    Update Navbar.jsx added logged in check when clicking messages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,6 +28,14 @@ const Navbar = () => {
     }
   };
 
+  const handleMessageClick = () => {
+    if (!isLoggedIn) {
+      navigate('/login');
+    } else {
+      navigate('/messages');
+    }
+  };
+
   return (
     <div className="Navbar bg-white flex justify-between items-center px-16 py-8 fixed top-0 left-0 right-0 z-50 shadow-md">
       <div className='text-2xl text-slate-600 font-black hover:scale-125 ease-in-out duration-300'>
@@ -44,7 +52,9 @@ const Navbar = () => {
         <Link className='hover:underline' to='/catalog'>Catalog</Link>
         <div className='text-slate-400'>|</div>
 
-        <Link className='hover:underline' to='/messages'>Messages</Link>
+        <button onClick={handleMessageClick} className='hover:underline'>
+          Messages
+        </button>
         <div className='text-slate-400'>|</div>
 
         <button onClick={handleProfileClick} className='hover:underline'>
