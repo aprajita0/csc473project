fed10d7d7401dd2728ebc9a0c572736b9f7d6245
Jezlea Ortega <124209539+XJezleaX@users.noreply.github.com>
2024-12-06 11:30:14 -0500

    Update App.jsx added route for messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './routes/LandingPage';
 import Login from './routes/Login'; 
 import Register from './routes/Register'; 
@@ -6,6 +6,7 @@ import Add_Card from './routes/Add_Card';
 import Catalog from './routes/Catalog';
 import TradingHub from './routes/TradingHub';
 import Messages from './routes/Messages';
+import Compose from './components/ComposeMessage';
 
 const App = () => {
   return (
@@ -15,13 +16,13 @@ const App = () => {
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/Add_Card" element={<Add_Card />} />
-          <Route path="/trading-hub" element={<TradingHub />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/messages" element={<Messages />} />
+          <Route path="/Compose" element={<Compose/>} />
+          <Route path="/messages" element={<Messages />} /> 
+          {/* <Route path="/trading-hub" element={<TradingHub />} />
+          <Route path="/catalog" element={<Catalog />} />*/}
         </Routes>
     </div>
   );
 }
 
 export default App;
-
