ba8f12a3a3b25ece1ffc7468835a23570502d9a6
zehdi02 <de.melchizedek.castro@gmail.com>
2024-12-11 14:37:23 -0500

    Added path '/tradepost/:id' for UserTradePost.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './routes/LandingPage';
-import Login from './routes/Login'; 
-import Register from './routes/Register'; 
-import Add_Card from './routes/Add_Card'; 
+import Login from './routes/Login';
+import Register from './routes/Register';
+import Add_Card from './routes/Add_Card';
 import Catalog from './routes/Catalog';
 import TradingHub from './routes/TradingHub';
 import Messages from './routes/Messages';
 import Compose from './components/ComposeMessage';
 import Team from './routes/Team';
+import UserTradePost from './routes/UserTradePost';
 
 const App = () => {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Add_Card" element={<Add_Card />} />
-          <Route path="/Compose" element={<Compose/>} />
-          <Route path="/messages" element={<Messages />} /> 
-          <Route path="/Team" element={<Team />} /> 
-          {/* <Route path="/trading-hub" element={<TradingHub />} />
-          <Route path="/catalog" element={<Catalog />} />*/}
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Add_Card" element={<Add_Card />} />
+        <Route path="/Compose" element={<Compose />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/trading-hub" element={<TradingHub />} />
+        <Route path="/tradepost/:id" element={<UserTradePost />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
     </div>
   );
 }
