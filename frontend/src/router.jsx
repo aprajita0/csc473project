f996764a0ab185f07438ebbeb5d0370834329254
zehdi02 <de.melchizedek.castro@gmail.com>
2024-12-11 14:36:41 -0500

    Added path '/tradepost/:id' for UserTradePost.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,29 +1,27 @@
-import { createBrowserRouter } from "react-router-dom"
-import App from "./App.jsx"
-import Catalog from './routes/Catalog.jsx'
-import TradingHub from './routes/TradingHub.jsx'
-import Messages from './routes/Messages.jsx'
-import UserProfile from './routes/UserProfile.jsx'
-import Login from './routes/Login.jsx'
-import Register from './routes/Register.jsx'
-import UserTradePost from "./routes/UserTradePost.jsx"
-import Add_Card from "./routes/Add_Card.jsx"
-import Compose from "./components/ComposeMessage.jsx"
-import Team from "./routes/Team.jsx"
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Catalog from './routes/Catalog.jsx';
+import TradingHub from './routes/TradingHub.jsx';
+import Messages from './routes/Messages.jsx';
+import UserProfile from './routes/UserProfile.jsx';
+import Login from './routes/Login.jsx';
+import Register from './routes/Register.jsx';
+import UserTradePost from "./routes/UserTradePost.jsx";
+import Add_Card from "./routes/Add_Card.jsx";
+import Compose from "./components/ComposeMessage.jsx";
+import Team from "./routes/Team.jsx";
 
 export const router = createBrowserRouter([
-    // NavBar
-    { path:'/', element:<App /> },
-    { path:'/catalog', element:<Catalog /> },
-    { path:'/trading-hub', element:<TradingHub /> },
-    { path:'/messages', element:<Messages /> },
-    { path:'/profile', element:<UserProfile /> },
-    { path:'/Login', element:<Login/> },
-    { path:'/Register', element:<Register/> },
-    { path:'/Add_Card', element:<Add_Card/> },
-    { path:'/Compose', element:<Compose/> },
-    { path:'/Team', element:<Team/> },
+  { path:'/', element:<App /> },
+  { path:'/catalog', element:<Catalog /> },
+  { path:'/trading-hub', element:<TradingHub /> },
+  { path:'/messages', element:<Messages /> },
+  { path:'/profile', element:<UserProfile /> },
+  { path:'/Login', element:<Login /> },
+  { path:'/Register', element:<Register /> },
+  { path:'/Add_Card', element:<Add_Card /> },
+  { path:'/Compose', element:<Compose /> },
+  { path:'/Team', element:<Team /> },
 
-    { path:'/tradepost', element:<UserTradePost /> },
-
-  ]);
+  { path:'/tradepost/:id', element:<UserTradePost /> }, 
+]);
