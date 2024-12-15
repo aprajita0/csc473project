8f4dc460f0c3ea6bf7683680cdd00e6571a52038
Aprajita Srivastava <aprajita0625@gmail.com>
2024-12-15 01:07:14 -0500

    changed backend server.js

--- server/server.js
+++ server/server.js
@@ -13,12 +13,11 @@ const app = express();
 const uri = process.env.MONGO_URI;
 const _port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
 
 app.use(express.json());
 
