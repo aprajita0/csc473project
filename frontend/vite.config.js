5140caef264fb6757a291dab444094b639e1c6f7
Aprajita Srivastava <aprajita0625@gmail.com>
2024-12-15 17:32:02 -0500

    Preparing for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'https://server-sooty-gamma-45.vercel.app/', 
+                target: 'http://localhost:5000', 
                 changeOrigin: true,
                 secure: true,
             },
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
+console.log("NODE_ENV:", process.env.NODE_ENV);
 
 const mongoose = require('mongoose');
 const express = require('express');
@@ -8,36 +9,42 @@ const userRoutes = require('./routes/userRoutes');
 const uploadRoutes = require('./routes/upload');
 const messageRoutes = require('./routes/messageRoutes');
 const commentRoutes = require('./routes/commentRoutes');
+const path = require('path');
 
 const app = express();
 const uri = process.env.MONGO_URI;
-const _port = process.env.PORT || 5000;
-
-app.use(cors({
-  origin: 'https://myidollist.vercel.app/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// Routes
+// API Routes
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/uploads', uploadRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  const distPath = path.join(__dirname, "../frontend/dist"); // Adjust this path to point to the correct location of frontend/dist
+  console.log(`Serving static files from: ${distPath}`); // Log the path to ensure it's correct
+  app.use(express.static(distPath)); // Serve static files from dist folder
 
-// Database Connection and Server Start
+  app.get('*', (req, res) => {
+    console.log('Redirecting to index.html'); // Debug redirect
+    res.sendFile(path.resolve(distPath, "index.html")); // Serve the index.html from the correct path
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running');
+  });
+}
+
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(_port, () => {
-      console.log(`Server is running on port ${_port}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((error) => {
