440f6bdfca9653a4da28f16371ad4df78bff5ba4
Aprajita Srivastava <aprajita0625@gmail.com>
2024-12-15 00:55:42 -0500

    chenged server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const _port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
